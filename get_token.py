323d09134c8dd47b4218c9cd5e48dc53fd343fb2
__patrick__ <krustenkaese@yandex.com>
2020-11-23 17:59:00 +0100

    Added comments

--- get_token.py
+++ get_token.py
@@ -1,13 +1,19 @@
 import pickle
 from google_auth_oauthlib.flow import InstalledAppFlow
 
+# Scope for which access is granted
+# see: https://developers.google.com/identity/protocols/oauth2/scopes#script
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-def main():
+def main() -> None:
+    """Save credentials for Google Sheets api to file token.pickle"""
+    # create a flow from Google Client secret file
     flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-    creds = flow.run_local_server(port=0)
+    # get token from flow
+    result = flow.run_local_server(port=0)
+    # save token to .pickle file
     with open('token.pickle', 'wb') as token:
-        pickle.dump(creds, token)
+        pickle.dump(result, token)
 
 if __name__ == '__main__':
     main()
