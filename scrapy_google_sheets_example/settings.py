373fb11c1c29f3a743ecc056830c51c23da4b6dd
__patrick__ <krustenkaese@yandex.com>
2020-11-23 19:59:15 +0100

    Added comments

--- scrapy_google_sheets_example/settings.py
+++ scrapy_google_sheets_example/settings.py
@@ -3,9 +3,13 @@ BOT_NAME = 'scrapy_google_sheets_example'
 SPIDER_MODULES = ['scrapy_google_sheets_example.spiders']
 NEWSPIDER_MODULE = 'scrapy_google_sheets_example.spiders'
 
+# id of the spreadsheet you want to use to store your data
 GOOGLE_SHEET_ID = '1eX8ftT1jKY2MyUcaHFV-Oo93qGP1NgHdcC-4MFm6UUc'
+# range in which to insert the data
+# the following specifies column A to column B
 GOOGLE_SHEET_RANGE = 'A:B'
 
+# activate the pipeline to transfer data to google sheets
 from .pipelines import GoogleSheetsPipeline
 ITEM_PIPELINES = {
     GoogleSheetsPipeline: 300
